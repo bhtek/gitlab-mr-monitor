281dd2048a35a090ca1a32187fc0e11b07084c7f
Boon Hian Tek <boon@m-daq.com>
2018-02-21 14:19:35 +0800

    Add ability to load data page by page.

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -10,6 +10,9 @@ import (
 	"strconv"
 	"sync"
 	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -17,12 +20,56 @@ const (
 )
 
 type GitLabAdapter struct {
-	ApiKey string
+	ApiKey  string
+	Session *mgo.Session
+	client  *http.Client
+}
+
+func NewGitLabAdapter(apiKey string, session *mgo.Session) GitLabAdapter {
+	client := &http.Client{
+		Timeout: time.Second * 60,
+	}
+	return GitLabAdapter{apiKey, session, client}
 }
 
 type GitLabProj struct {
-	Id   uint32
-	Name string
+	Id                uint32
+	Name              string
+	NameWithNamespace string `json:"name_with_namespace"`
+}
+
+type GitLabMergeRequest struct {
+	_Id          string
+	Id           uint32
+	Iid          uint32
+	ProjectId    uint32    `json:"project_id"`
+	Title        string
+	State        string
+	Upvotes      int
+	Downvotes    int
+	CreatedAt    time.Time `json:"created_at"`
+	TargetBranch string    `json:"target_branch"`
+	SourceBranch string    `json:"source_branch"`
+	WebUrl       string    `json:"web_url"`
+	Author       GitLabUser
+	Assignee     GitLabUser
+	Emojis       []GitLabEmoji
+}
+
+type GitLabUser struct {
+	Id       uint32
+	Name     string
+	Username string
+}
+
+type GitLabEmoji struct {
+	Id            uint32
+	Name          string
+	User          GitLabUser
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	AwardableId   uint32    `json:"awardable_id"`
+	AwardableType string    `json:"awardable_type"`
 }
 
 func (gla GitLabAdapter) SearchProjects() ([]GitLabProj, error) {
@@ -112,4 +159,150 @@ func (gla GitLabAdapter) fetch(client *http.Client, url *url2.URL, page int, pro
 
 	fmt.Println("Done for call of page ", page)
 	wg.Done()
+}
+
+func (gla GitLabAdapter) LoadMergeRequests(projectId int, mrc *mgo.Collection, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	url, err := url2.Parse(baseUrl + "/projects/" + strconv.Itoa(projectId) + "/merge_requests?sort=asc")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	lastMergeRequest := GitLabMergeRequest{}
+	type list []interface{}
+	var lastCreatedAt time.Time
+	err = mrc.Find(bson.M{"projectid": projectId, "state": bson.M{"$nin": list{"closed", "merged"}}}).Sort("createdat").One(&lastMergeRequest)
+	if err != nil {
+		err = mrc.Find(bson.M{"projectid": projectId, "state": bson.M{"$in": list{"closed", "merged"}}}).Sort("-createdat").One(&lastMergeRequest)
+	}
+	if err != nil {
+		fmt.Printf("Failed to find last merge for projectid=%d due to %s.\n\t%s\n", projectId, err, url)
+	} else {
+		lastCreatedAt = lastMergeRequest.CreatedAt
+		fmt.Printf("Will look for merge requests for projectid=%d after %s.\n", projectId, lastCreatedAt)
+	}
+
+	query := url.Query()
+	if !lastCreatedAt.IsZero() {
+		query.Add("created_after", lastCreatedAt.String())
+	}
+
+	var pageNumber int = 1
+	var totalMods int
+	for {
+		mods, has_more, err := gla.loadMergeRequestPage(pageNumber, url, &query, mrc)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		totalMods += mods
+
+		if !has_more {
+			break
+		}
+
+		pageNumber += 1
+	}
+
+	fmt.Printf("Handled %d merge request(s) for projectId=%d.\n", totalMods, projectId)
+}
+
+func (gla GitLabAdapter) loadMergeRequestPage(pageNumber int, url *url2.URL, query *url2.Values, mrc *mgo.Collection) (int, bool, error) {
+	query.Set("page", strconv.Itoa(pageNumber))
+
+	url.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return 0, false, err
+	}
+
+	req.Header.Add("PRIVATE-TOKEN", gla.ApiKey)
+
+	resp, err := gla.client.Do(req)
+
+	nextPage := resp.Header.Get("X-Next-Page")
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var mergeRequests []GitLabMergeRequest
+	err = json.Unmarshal(response, &mergeRequests)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Failed to unmarshal for url=%d: %s\n", url, err))
+		return 0, false, err
+	}
+
+	var mods int = 0
+
+	for _, mergeRequest := range mergeRequests {
+		var mrdb GitLabMergeRequest
+		err := mrc.Find(bson.M{"id": mergeRequest.Id}).One(&mrdb)
+		if err != nil {
+			if (mergeRequest.State != "opened") {
+				err = gla.enrichMergeRequest(&mergeRequest)
+				if err != nil {
+					return 0, false, err
+				}
+			}
+			mods += 1
+			mrc.Insert(mergeRequest)
+			//fmt.Printf("%s\n", mergeRequest)
+		} else {
+			if mrdb.State == "opened" && mergeRequest.State != "opened" {
+				gla.enrichMergeRequest(&mergeRequest)
+				if err != nil {
+					return 0, false, err
+				}
+
+				mods += 1
+				mrc.Update(bson.M{"id": mergeRequest.Id}, mergeRequest)
+				//fmt.Printf("Updated mr %d to state=%s\n", mergeRequest.Id, mergeRequest.State)
+			} else {
+				//fmt.Printf("Skipped mr %d @ state=%s\n", mergeRequest.Id, mergeRequest.State)
+			}
+		}
+	}
+
+	if nextPage != "" {
+		return mods, true, nil
+	} else {
+		return mods, false, nil
+	}
+}
+
+func (gla GitLabAdapter) enrichMergeRequest(mr *GitLabMergeRequest) error {
+	url, err := url2.Parse(baseUrl + "/projects/" + fmt.Sprint(mr.ProjectId) + "/merge_requests/" +
+		fmt.Sprint(mr.Iid) + "/award_emoji")
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("PRIVATE-TOKEN", gla.ApiKey)
+
+	resp, err := gla.client.Do(req)
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var emojis []GitLabEmoji
+	err = json.Unmarshal(response, &emojis)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mr.Emojis = emojis
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"sync"
 
 	"./adapter"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
-	adapter := adapter.GitLabAdapter{ApiKey: "By3ySyMr-9S5yD3zDYsu"}
-	projs, err := adapter.SearchProjects()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//projs, err := adapter.SearchProjects()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//filters := []string{"aladdin", "tms"}
+	//
+	//for _, proj := range projs {
+	//	selected := false
+	//	if strings.Index(proj.NameWithNamespace, "consul") < 0 {
+	//	FilterLoop:
+	//		for _, filter := range filters {
+	//			if strings.Index(proj.NameWithNamespace, filter) > -1 {
+	//				selected = true
+	//				break FilterLoop
+	//			}
+	//		}
+	//	}
+	//
+	//	if selected {
+	//		fmt.Println(proj)
+	//	}
+	//}
 
-	for _, proj := range projs {
-		fmt.Println(proj)
+	proj_ids := []int{11, 12, 14, 15, 17, 19, 23, 27, 303, 34, 202, 228, 231, 235, 340, 36, 39, 413, 43, 511, 603}
+	//proj_ids := []int{511}
+
+	session, err := mgo.Dial("172.17.0.2")
+	if err != nil {
+		panic(err)
 	}
+	defer session.Close()
+
+	mrc := session.DB("mrm").C("mergerequests")
+
+	adptr := adapter.NewGitLabAdapter("By3ySyMr-9S5yD3zDYsu", session)
+
+	var wg sync.WaitGroup
+	for _, proj_id := range proj_ids {
+		wg.Add(1)
+		go adptr.LoadMergeRequests(proj_id, mrc, &wg)
+	}
+	wg.Wait()
 }
