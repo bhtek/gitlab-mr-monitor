3d84580b931a8aa5eac0b6ea9aaf700e09b4c4e2
Boon Hian Tek <boon@m-daq.com>
2018-02-21 14:19:56 +0800

    Clean up.

--- main.go
+++ main.go
@@ -8,30 +8,6 @@ import (
 )
 
 func main() {
-	//projs, err := adapter.SearchProjects()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//filters := []string{"aladdin", "tms"}
-	//
-	//for _, proj := range projs {
-	//	selected := false
-	//	if strings.Index(proj.NameWithNamespace, "consul") < 0 {
-	//	FilterLoop:
-	//		for _, filter := range filters {
-	//			if strings.Index(proj.NameWithNamespace, filter) > -1 {
-	//				selected = true
-	//				break FilterLoop
-	//			}
-	//		}
-	//	}
-	//
-	//	if selected {
-	//		fmt.Println(proj)
-	//	}
-	//}
-
 	proj_ids := []int{11, 12, 14, 15, 17, 19, 23, 27, 303, 34, 202, 228, 231, 235, 340, 36, 39, 413, 43, 511, 603}
 	//proj_ids := []int{511}
 
